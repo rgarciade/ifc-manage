afac461a918420910201bd2c688561cae824a667
raul garcia de la fuente <garcia.de.la.fuente.raul@gmail.com>
2024-09-02 13:24:04 +0200

    feat: create class right panel

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as BUI from "@thatopen/ui";
 import { GenerateWorld } from "./generateWorld";
 import { InitIfLoader } from "./ifLoader";
 import { FragmentsGroup } from "@thatopen/fragments";
+import {rightPanel} from "./ui/rightPanel";
 
 const standardWorld = new GenerateWorld();
 const ifLoader = new InitIfLoader(standardWorld);
@@ -41,97 +42,156 @@ const exportFragments = () => {
 function disposeFragments() {
   ifLoader.fragments.dispose();
 }
-
-// function fitLastModel(){
-//   const fragmentBbox = components.get(OBC.BoundingBoxer);
-// fragmentBbox.add(lastModel;
-// }
-
+debugger;
 BUI.Manager.init();
-
-const panel = BUI.Component.create<BUI.PanelSection>(() => {
-  return BUI.html`
-  <bim-panel active label="IFC Loader Tutorial" class="options-menu">
-    <bim-panel-section collapsed label="Charge ifc">
-      <bim-panel-section style="padding-top: 12px;">
-     
-        <bim-button label="Load small 1 710KB IFC"
-          @click="${async () => {
-            await loadIfc(`01`);
-          }}">
-        </bim-button>
-        <bim-button label="Load small 2 15,2M IFC"
-          @click="${async () => {
-            await loadIfc(`03`);
-          }}">
-           </bim-button>  
-        <bim-button label="Load 240717MAD03-STRC-DH-TEC-R24 19M IFC"
-          @click="${async () => {
-            await loadIfc(`240717MAD03-STRC-DH-TEC-R24`);
-          }}">
-        </bim-button>  
-        <bim-button label="Load BSA1X - bausa 13-15 240122 431M IFC"
-          @click="${async () => {
-            await loadIfc(`BSA1X - bausa 13-15 240122`);
-          }}">
-        </bim-button>  
-        <bim-button label="example project location"
-          @click="${async () => {
-            await loadIfc(`example project location`);
-          }}">
-        </bim-button>  
-       
-  <bim-button label="Load EncofradoVigaPlasencia 837k IFC"
-          @click="${async () => {
-            await loadIfc(`EncofradoVigaPlasencia`);
-          }}">
-        </bim-button>  
-         <bim-button label="Fit last model"
-          @click="${() => {
-            standardWorld.fitLastModel();
-          }}">
-        </bim-button>
-            
-        <bim-button label="Export fragments"
-          @click="${() => {
-            exportFragments();
-          }}">
-        </bim-button>  
-            
-        <bim-button label="Dispose fragments"
-          @click="${() => {
-            disposeFragments();
-          }}">
-        </bim-button>
-         <bim-button label="active culler"
-          @click="${() => {
-            ifLoader.toggleCuller();
-          }}">
-        </bim-button>
-      
-      </bim-panel-section>
-    </bim-panel>
-  `;
-});
-
-document.body.append(panel);
+new rightPanel([
+    {
+        label: 'FC Loader Tutorial',
+        collapsed: false,
+        elements: [
+            {
+                type: 'button',
+                label: 'Load small 1 710KB IFC',
+                function: () => loadIfc(`01`),
+            },
+            {
+                type: 'button',
+                label: 'Load small 2 15,2M IFC',
+                function: () => loadIfc(`03`),
+            },
+            {
+                type: 'button',
+                label: 'Load 240717MAD03-STRC-DH-TEC-R24 19M IFC',
+                function: () => loadIfc(`240717MAD03-STRC-DH-TEC-R24`),
+            },
+            {
+                type: 'button',
+                label: 'Load BSA1X - bausa 13-15 240122 431M IFC',
+                function: () => loadIfc(`BSA1X - bausa 13-15 240122`),
+            },
+            {
+                type: 'button',
+                label: 'example project location',
+                function: () => loadIfc(`example project location`),
+            },
+            {
+                type: 'button',
+                label: 'Load EncofradoVigaPlasencia 837k IFC',
+                function: () => loadIfc(`EncofradoVigaPlasencia`),
+            }
+        ]
+    },
+    {
+        label: 'Actions',
+        collapsed: false,
+        elements: [
+            {
+                type: 'button',
+                label: 'Fit last model',
+                function: () => standardWorld.fitLastModel(),
+            },
+            {
+                type: 'button',
+                label: 'Export fragments',
+                function: () => exportFragments(),
+            },
+            {
+                type: 'button',
+                label: 'Dispose fragments',
+                function: () => disposeFragments(),
+            },
+            {
+                type: 'button',
+                label: 'active culler',
+                function: () => ifLoader.toggleCuller(),
+            }
+        ]
+    },
+]);
+//
+// const panel = BUI.Component.create<BUI.PanelSection>(() => {
+//   return BUI.html`
+//   <bim-panel active label="IFC Loader Tutorial" class="options-menu">
+//     <bim-panel-section collapsed label="Charge ifc">
+//       <bim-panel-section style="padding-top: 12px;">
+//
+//         <bim-button label="Load small 1 710KB IFC"
+//           @click="${async () => {
+//             await loadIfc(`01`);
+//           }}">
+//         </bim-button>
+//         <bim-button label="Load small 2 15,2M IFC"
+//           @click="${async () => {
+//             await loadIfc(`03`);
+//           }}">
+//            </bim-button>
+//         <bim-button label="Load 240717MAD03-STRC-DH-TEC-R24 19M IFC"
+//           @click="${async () => {
+//             await loadIfc(`240717MAD03-STRC-DH-TEC-R24`);
+//           }}">
+//         </bim-button>
+//         <bim-button label="Load BSA1X - bausa 13-15 240122 431M IFC"
+//           @click="${async () => {
+//             await loadIfc(`BSA1X - bausa 13-15 240122`);
+//           }}">
+//         </bim-button>
+//         <bim-button label="example project location"
+//           @click="${async () => {
+//             await loadIfc(`example project location`);
+//           }}">
+//         </bim-button>
+//
+//         <bim-button label="Load EncofradoVigaPlasencia 837k IFC"
+//           @click="${async () => {
+//             await loadIfc(`EncofradoVigaPlasencia`);
+//           }}">
+//         </bim-button>
+//          <bim-button label="Fit last model"
+//           @click="${() => {
+//             standardWorld.fitLastModel();
+//           }}">
+//         </bim-button>
+//
+//         <bim-button label="Export fragments"
+//           @click="${() => {
+//             exportFragments();
+//           }}">
+//         </bim-button>
+//
+//         <bim-button label="Dispose fragments"
+//           @click="${() => {
+//             disposeFragments();
+//           }}">
+//         </bim-button>
+//          <bim-button label="active culler"
+//           @click="${() => {
+//             ifLoader.toggleCuller();
+//           }}">
+//         </bim-button>
+//
+//       </bim-panel-section>
+//     </bim-panel>
+//   `;
+// });
+//
+// document.body.append(panel);
 
 /* MD
   And we will make some logic that adds a button to the screen when the user is visiting our app from their phone, allowing to show or hide the menu. Otherwise, the menu would make the app unusable.
 */
 
-const button = BUI.Component.create<BUI.PanelSection>(() => {
-  return BUI.html`
-      <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
-        @click="${() => {
-          if (panel.classList.contains("options-menu-visible")) {
-            panel.classList.remove("options-menu-visible");
-          } else {
-            panel.classList.add("options-menu-visible");
-          }
-        }}">
-      </bim-button>
-    `;
-});
+// const button = BUI.Component.create<BUI.PanelSection>(() => {
+//   return BUI.html`
+//       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
+//         @click="${() => {
+//           if (panel.classList.contains("options-menu-visible")) {
+//             panel.classList.remove("options-menu-visible");
+//           } else {
+//             panel.classList.add("options-menu-visible");
+//           }
+//         }}">
+//       </bim-button>
+//     `;
+// });
 
-document.body.append(button);
+//document.body.append(button);
---
+++ src/ui/rightPanel.ts
@@ -0,0 +1,69 @@
+import * as BUI from "@thatopen/ui";
+BUI.Manager.init();
+
+interface Element {
+  label: string;
+  function: () => void;
+}
+const elementTypes = ["button", "checkbox"] as const;
+// Definición de la interfaz Button que extiende de Element
+interface Button extends Element {
+  type: "button";
+}
+
+// Definición de la interfaz Checkbox que extiende de Element
+interface Checkbox extends Element {
+  type: "checkbox";
+  checked?: boolean;
+}
+
+// Definición de la interfaz Section que contiene un array de elementos
+interface Section {
+  label: string;
+  collapsed: boolean;
+  elements: (Button | Checkbox)[];
+}
+
+export class rightPanel {
+  sections: Section[];
+  panel: BUI.PanelSection;
+
+  constructor(sections: Section[]) {
+    this.sections = sections;
+    this.panel = this.generatePanel();
+    document.body.append(this.panel);
+  }
+  private generateSection(section: Section) {
+    let elements: string [] = []
+
+    const collapsedSectionsHtml = (sectionsHtml:string []) => BUI.html`<bim-panel-section collapsed label="${section.label}"><bim-panel-section style="padding-top: 12px;">${sectionsHtml.map(
+        (sectionHtml) => sectionHtml
+    )}</bim-panel-section></bim-panel-section>`
+    const singleSectionHtml = (sectionsHtml:string []) => BUI.html`<bim-panel-section label="${section.label}">${sectionsHtml.map(
+        (sectionHtml) => sectionHtml
+    )}</bim-panel-section>`
+
+    const elementHtmlGenerators = {
+      button: (element: Button) => BUI.html`<bim-button label="${element.label}" @click="${ () => {element.function();}}"></bim-button>`,
+      checkbox: (element: Checkbox) => BUI.html`<bim-checkbox label="${element.label}" checked="${element.checked}" @change="${async () => {element.function();}}"></bim-checkbox>`
+    };
+
+
+    section.elements.map((element) => {
+      if(elementTypes.includes(element.type)) {
+        // @ts-ignore
+        elements.push(elementHtmlGenerators[element.type](element));
+      }
+    });
+
+    return section.collapsed ? collapsedSectionsHtml(elements) : singleSectionHtml(elements);
+
+  }
+  private generatePanel() {
+    return BUI.Component.create<BUI.PanelSection>(() => BUI.html`
+          <bim-panel active label="Ferro Ifc panel" class="options-menu">
+            ${this.sections.map((section) => this.generateSection(section))}
+          </bim-panel>
+        `);
+  }
+}
